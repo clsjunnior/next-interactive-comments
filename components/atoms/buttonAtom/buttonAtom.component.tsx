7c35dc75b64f4992e1a888c0c3f217b9a2bf41c1
Celso junior <clsjunnior@live.com>
2023-07-31 23:36:37 -0300

    feat: buttonAtom

--- components/atoms/buttonAtom/buttonAtom.component.tsx
+++ components/atoms/buttonAtom/buttonAtom.component.tsx
@@ -4,20 +4,29 @@ import React, { ButtonHTMLAttributes } from 'react'
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
   colorType?: 'blue' | 'danger' | 'grayish'
+  isLink?: boolean
 }
 
 const ButtonAtom: React.FC<ButtonProps> = ({
   colorType = 'blue',
+  isLink = false,
   children,
   ...rest
 }) => {
   const btnClass = clsx(
     'hover:opacity-80 transition-opacity text-white py-2 px-4 rounded',
-    {
-      'bg-moderate-blue': colorType === 'blue',
-      'bg-soft-red': colorType === 'danger',
-      'bg-grayish-blue': colorType === 'grayish',
-    }
+    isLink
+      ? {
+          'bg-transparent': isLink,
+          'text-moderate-blue': colorType === 'blue',
+          'text-soft-red': colorType === 'danger',
+          'text-grayish-blue': colorType === 'grayish',
+        }
+      : {
+          'bg-moderate-blue': colorType === 'blue',
+          'bg-soft-red': colorType === 'danger',
+          'bg-grayish-blue': colorType === 'grayish',
+        }
   )
 
   return (
